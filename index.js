a97a8e182d3f37297c0f1b8365532a5aea946190
Winnie Magoma <40880690+winniebosy@users.noreply.github.com>
2022-02-24 08:02:02 +0300

    comment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+document.addEventListener('keydown', event =>  {
+  var element = document.querySelector('p');
+var a = 'KeyA';
+var s = 'KeyS';
+var d = 'KeyD';
+var w = 'KeyW';
 
+switch (event.code) {
+  case a :
+    element.textContext = 'Left';
+  case s :
+    elemement.textContent ="Down";
+    
+  case d :
+    element.textContent = 'Right';
+    
+  case W:
+    element.textContent = 'Up';
+    break;
+    
+    
+}
+});
+                          
+                          
+                          
+//the above code shows how one can create keyboard events to correspond to each key press
